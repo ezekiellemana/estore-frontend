75a9db5cf5c199e34ab86018252a443e904b03d5
EnzoLeman <ezekielaugustino@gmail.com>
2025-06-09 14:01:46 +0300

    fix: baseURL

--- src/services/api.js
+++ src/services/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 import { toast } from 'react-toastify';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:5000',
+  baseURL: import.meta.env.VITE_API_URL || 'https://estore-backend-if2a.onrender.com',
   withCredentials: true,              // ← allow cookies (guestId)
   headers: { 'Content-Type': 'application/json' },
 });
