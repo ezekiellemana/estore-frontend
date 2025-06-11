fe4251a4cab95ca94572d23f84b489ac600ee5fe
EnzoLeman <ezekielaugustino@gmail.com>
2025-06-11 14:53:53 +0300

    fix

--- vite.config.js
+++ vite.config.js
@@ -6,9 +6,24 @@ import path from 'path';
 export default defineConfig({
   plugins: [react()],
   resolve: {
-    alias: {
-      // make `@/` map to your `/src` folder
-      '@': path.resolve(__dirname, './src'),
+    alias: { '@': path.resolve(__dirname, './src') },
+  },
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('html2canvas') || id.includes('jspdf')) {
+              return 'pdf-html2canvas';
+            }
+            if (id.includes('recharts') || id.includes('chart.js')) {
+              return 'charts';
+            }
+            return 'vendor'; // everything else in node_modules
+          }
+        },
+      },
     },
+    chunkSizeWarningLimit: 1000, // bump the warning threshold if you want
   },
 });
